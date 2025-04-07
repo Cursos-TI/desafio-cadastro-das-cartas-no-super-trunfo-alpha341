259873bbf83e39fc54c884507cfde9f0b6ea517d
alpha341 <141239314+alpha341@users.noreply.github.com>
2025-04-07 16:58:33 +0000

    VERSÃO 3 - CLÁCULO DE DENSIDADE POPULACIONAL E PIB PER CAPITA

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,12 +3,13 @@
 
 // Desafio Super Trunfo - Estados
 // Nível Novato - Cadastro de duas cartas
+// Nível Aventureiro - Cálculo de densidade populacional e PIB per capita
 /* Nesta parte, o programa dará a possibilidade do usuário cadastrar duas cartas. Cada uma delas 
 terá informações referentes a uma cidade*/
 // Teste Alex Lima
 
 int main() {
-    /* Passo 1 - Definir as variáveis da cidade, como: código, nome, população, área, PIB
+    /* Definir as variáveis da cidade, como: código, nome, população, área, PIB
     e número de pontos turísticos */
 
     char codigoEstado1; /* A ser preenchida com as letras de A a H, sendo que cada uma representa 
@@ -33,11 +34,20 @@ int main() {
     int pontosTuristicos2; /* Representa o número de pontos turísticos na cidade - Carta 2*/
 
 
+
+    /* Adicionado no nível aventureiro - Variáveis para comportar densidade populacional e 
+    PIB per capita */
+    float densidadePopulacional1;
+    float pibCapita1;
+    float densidadePopulacional2;
+    float pibCapita2;
+
+
     /* Agora serão feitas as solicitações de entrada, para a carta 1, para os usuários. 
     Após cada solicitação, o programa guarda o valor fornecido, pelo usuário, na variável
     correspondente */
 
-    printf("Insira o código do estado para carta 1 (uma letra entre A e H que represente o estado):");
+    printf("Insira o código do estado para carta 1 (uma letra entre A e H que não se repita):");
     scanf("%c", &codigoEstado1);
     getchar();
     printf("Insira o código da carta para carta 1 (iniciado com a letra que representa o estado e depois um número):");
@@ -50,27 +60,35 @@ int main() {
     scanf("%i", &populacao1);
     printf("Insira a área da cidade para carta 1(em km2):");
     scanf("%f", &area1);
-    printf("Insira o PIB da ciade para carta 1:");
+    printf("Insira o PIB da ciade para carta 1 (em reais):");
     scanf("%f", &pib1);
     printf("Insira a quantidade de pontos turísticos da cidade para carta 1:");
     scanf("%i", &pontosTuristicos1);
+
+    /* Adicionado no nível aventureiro - Cálculo de densidade populacional e PIB per capita */
+    densidadePopulacional1 = (float) populacao1/area1;
+    pibCapita1 = (float) pib1/populacao1;
     
 
-    /* Agora serão informadas as saídas, para a carta 1*/
+    //Agora serão informadas as saídas, para a carta 1//
     printf("\nCódigo de estado (carta 1): %c \n", codigoEstado1);
     printf("Código de carta (carta 1): %s \n", codigoCarta1);
     printf("Cidade (carta 1): %s\n", nomeCidade1);
     printf("Habitantes (carta 1): %i\n", populacao1);
-    printf("Área em km2 (carta 1) %.2f\n", area1);
-    printf("PIB (carta 1): %.2f\n", pib1);
-    printf("Quantidade de pontos turísticos (carta 1): %i\n\n\n", pontosTuristicos1);
+    printf("Área em km2 (carta 1) %.2f km2\n", area1);
+    printf("PIB em reais(carta 1): %.2f de reais\n", pib1);
+    printf("Quantidade de pontos turísticos (carta 1): %i\n", pontosTuristicos1);
+    //Adicionado no nível aventureiro - Densidade populacional e PIB per capita//
+    printf("Densidade populacional (carta 1): %.2f hab/km2\n", densidadePopulacional1);
+    printf("PIB per capita (carta 1): %.2f reais\n\n\n", pibCapita1);
+
 
     /* Agora serão feitas as solicitações de entrada, para a carta 2, para os usuários. 
     Após cada solicitação, o programa guarda o valor fornecido, pelo usuário, na variável
     correspondente */
 
     getchar();
-    printf("Insira o código do estado para carta 2 (uma letra entre A e H que represente o estado):");
+    printf("Insira o código do estado para carta 2 (uma letra entre A e H que não se repita):");
     scanf("%c", &codigoEstado2);
     getchar();
     printf("Insira o código da carta para carta 2 (iniciado com a letra que representa o estado e depois um número):");
@@ -83,20 +101,28 @@ int main() {
     scanf("%i", &populacao2);
     printf("Insira a área da cidade para carta 2(em km2):");
     scanf("%f", &area2);
-    printf("Insira o PIB da ciade para carta 2:");
+    printf("Insira o PIB da ciade para carta 2 (em reais):");
     scanf("%f", &pib2);
     printf("Insira a quantidade de pontos turísticos da cidade para carta 2:");
     scanf("%i", &pontosTuristicos2);
+
+
+    /* Adicionado no nível aventureiro - Cálculo de densidade populacional e PIB per capita */
+    densidadePopulacional2 = (float) populacao2/area2;
+    pibCapita2 = (float) pib2/populacao2;
     
 
-    /* Agora serão informadas as saídas, para a carta 1*/
+    // Agora serão informadas as saídas, para a carta 1//
     printf("\nCódigo de estado (carta 2): %c \n", codigoEstado2);
     printf("Código de carta (carta 2): %s \n", codigoCarta2);
     printf("Cidade (carta 2): %s\n", nomeCidade2);
     printf("Habitantes (carta 2): %i\n", populacao2);
-    printf("Área em km2 (carta 2) %.2f\n", area2);
-    printf("PIB (carta 2): %.2f\n", pib2);
-    printf("Quantidade de pontos turísticos (carta 2): %i\n\n\n", pontosTuristicos2);
+    printf("Área em km2 (carta 2) %.2f km2\n", area2);
+    printf("PIB em reais (carta 2): %.2f de reais\n", pib2);
+    printf("Quantidade de pontos turísticos (carta 2): %i\n", pontosTuristicos2);
+    //Adicionado no nível aventureiro - Densidade populacional e PIB per capita//
+    printf("Densidade populacional (carta 2): %.2f hab/km2\n", densidadePopulacional2);
+    printf("PIB per capita (carta 2): %.2f reais\n\n\n", pibCapita2);
 
 
     return 0;
